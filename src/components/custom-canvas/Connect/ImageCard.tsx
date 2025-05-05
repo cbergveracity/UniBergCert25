9cd65d6e64b803dbd086475279a8ddf52f029c7f
cberg <cberg@meetveracity.com>
2025-05-05 18:06:18 -0400

    Updating news listing, news detail

--- src/components/custom-canvas/Connect/ImageCard.tsx
+++ src/components/custom-canvas/Connect/ImageCard.tsx
@@ -11,6 +11,7 @@ import Image from 'next/image';
 
 type ImageCardParameters = {
   title: string;
+  url: string;
   image: Asset[];
 };
 
@@ -21,31 +22,33 @@ enum ImageCardSlots {
 }
 type ImageCardProps = ComponentProps<ImageCardParameters, ImageCardSlots>;
 
-export const ImageCardComponent: FC<ImageCardProps> = ({title, image, component, context, slots }) => {
+export const ImageCardComponent: FC<ImageCardProps> = ({title, url, image, component, context, slots }) => {
 
   const [resolvedImage] = resolveAsset(image);
 
   return (
-    <div className="flex flex-col overflow-hidden shadow-md border border-gray-200 h-[30rem]">
-      {/* Image section - fixed height of 20rem (320px) */}
-      <div className="h-80 overflow-hidden">
-        <Image
-          src={resolvedImage.url}
-          alt={title}
-          width={400}
-          height={320}
-          className="w-full h-full object-cover"
-        />
-      </div>
+    <Link href={url || " "} className="block h-[35rem] cursor-pointer">
+      <div className="flex flex-col overflow-hidden shadow-md border border-gray-200 h-full transition-shadow hover:shadow-lg">
+        {/* Image section - fixed height of 20rem (320px) */}
+        <div className="h-80 overflow-hidden">
+          <Image
+            src={resolvedImage.url}
+            alt={title}
+            width={400}
+            height={320}
+            className="w-full h-full object-cover"
+          />
+        </div>
 
-      {/* Content section - also fixed height of 20rem (320px) */}
-      <div className="flex flex-col justify-between p-6 h-40">
-        <h3 className="text-xl font-semibold mb-4 line-clamp-3">{title}</h3>
-        <span className="text-gray-500 mt-auto">
-          <UniformSlot data={component} context={context} slot={slots.imageCardBottom} />
-        </span>
+        {/* Content section - also fixed height of 20rem (320px) */}
+        <div className="flex flex-col justify-between p-6 h-60">
+          <h3 className="text-xl font-semibold mb-4 line-clamp-3">{title}</h3>
+          <span className="text-gray-500 mt-auto">
+            <UniformSlot data={component} context={context} slot={slots.imageCardBottom} />
+          </span>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
